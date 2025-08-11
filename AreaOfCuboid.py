e406a88c0e808ff68248dcc62188abb4a3e16ee9
ketandeepakkulkarni <kulkarni.kd16@gmail.com>
2025-08-11 13:07:29 +0530

    Done

---
+++ AreaOfCuboid.py
@@ -0,0 +1,15 @@
+#This program is about to find the area of cuboid
+
+print('Area of Cuboid: ')
+l = int(input('Enter length: '))
+
+b = int(input('Enter breadth: '))
+
+h = int(input('Enter height: '))
+
+area = 2*(l * b + l * h + b * h)
+print('Area: ', area)
+
+print('Length: ', l)
+print('Breadth: ', b)
+print('Height: ', h)
