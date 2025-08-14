89dca69e9beab2089aa8591984a2ff056dfb9087
ketandeepakkulkarni <kulkarni.kd16@gmail.com>
2025-08-14 13:02:25 +0530

    Basic Program

--- PositiveOrNegative.py
+++ PositiveOrNegative.py
@@ -1,7 +1,7 @@
 #This program is about to check the number is positive or negative
 
 number = int(input('Enter the number: '))
-if( number > 0):
+if number > 0:
     print('The number is Positive', number)
 else:
     print('The number is negative: ', number)
