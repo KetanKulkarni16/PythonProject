67ce285c8f4a8f11a4ba418b99bf509c7fef578f
ketandeepakkulkarni <kulkarni.kd16@gmail.com>
2025-08-20 12:10:49 +0530

    Basic Program

---
+++ EligibleToVote.py
@@ -0,0 +1,7 @@
+#This program is about to find if the candidate is eligible to vote
+
+age = int(input('Enter the age of candidate: '))
+if age >= 18:
+    print('Eligible to vote')
+else:
+    print('Not eligible to vote')
---
+++ OddOrEven.py
@@ -0,0 +1,7 @@
+#This program is about to find the number is even or odd
+
+number = int(input('Enter the number: '))
+if number % 2 == 0:
+    print('The number is even')
+else:
+    print('The number is odd')
