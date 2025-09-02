4a74cf3515135b61bd55aae9fd401dc3c1ffac2d
ketandeepakkulkarni <kulkarni.kd16@gmail.com>
2025-09-02 11:36:53 +0530

    Basic Program

---
+++ FactorsOfNumber.py
@@ -0,0 +1,7 @@
+# This program is about to find the factors fo the numbers
+
+number = int(input("Enter the number to find factor: "))
+
+for i in range(1, number+1):
+    if number % i == 0:
+        print(i)
---
+++ PrimeNumber.py
@@ -0,0 +1,13 @@
+# This program is about to find the prime number
+
+number = int(input("Enter a number: "))
+count = 0
+
+for i in range(1, number+1):
+    if number % i == 0:
+        count += 1
+
+if count == 2:
+    print("Prime")
+else:
+    print("Not prime")
