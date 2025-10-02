b3bd2f5580c30da741ecbc538074a1640440bdc4
ketandeepakkulkarni <kulkarni.kd16@gmail.com>
2025-10-02 11:09:28 +0530

    Basic Program

---
+++ AvgOfTwoFloatNumbers.py
@@ -0,0 +1,8 @@
+# This program is about to print the average of two floating point numbers
+
+num1 = float(input("Enter a number1:"))
+num2 = float(input("Enter a number2:"))
+
+avg = (num1+num2) / 2
+
+print("The avg of two numbers: ", avg)
