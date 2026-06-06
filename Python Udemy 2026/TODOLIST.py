4ca791be08c7b17744fbc24420c96ef10d7b9f71
ketandeepakkulkarni <kulkarni.kd16@gmail.com>
2026-06-06 16:23:13 +0530

    Udemy 2026

---
+++ Python Udemy 2026/TODOLIST.py
@@ -0,0 +1,15 @@
+#This project is about to create a to-do list in python
+
+# print("Enter
+# user_text=input()
+# print(user_text)
+
+user_prompt = 'Enter todo:'
+todo1 = input(user_prompt)
+todo2 = input(user_prompt)
+todo3 = input(user_prompt)
+
+todos = [todo1, todo2, todo3]
+print(todos)
+
+print(type(user_prompt))
---
+++ Python Udemy 2026/bonus1.py
@@ -0,0 +1,6 @@
+#This program is about to print the title length of the book
+
+text = input("Enter a title: ")
+
+lenght = len(text)
+print("The lenght of the title: ",lenght)
