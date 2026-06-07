90e259ca17bac017715bb2c8b2612b9d967df345
ketandeepakkulkarni <kulkarni.kd16@gmail.com>
2026-06-07 10:14:24 +0530

    Udemy 2026

--- Python Udemy 2026/TODOLIST.py
+++ Python Udemy 2026/TODOLIST.py
@@ -5,11 +5,10 @@
 # print(user_text)
 
 user_prompt = 'Enter todo:'
-todo1 = input(user_prompt)
-todo2 = input(user_prompt)
-todo3 = input(user_prompt)
 
-todos = [todo1, todo2, todo3]
-print(todos)
+todos = []
 
-print(type(user_prompt))
+while True :
+    todo = input(user_prompt)
+    print(todo.capitalize())
+    todos.append(todo)
