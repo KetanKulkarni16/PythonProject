4aff59be3325b0c622cbe06f8535f8f21b749043
ketandeepakkulkarni <kulkarni.kd16@gmail.com>
2026-07-25 17:50:33 +0530

    Udemy 2026 New Short Course

---
+++ Python YouTube/One.py
@@ -0,0 +1,46 @@
+# print("Hello ketan")
+# print("Test")
+#
+# #Ask the user name
+# user_name = input("What is your name? ")
+# #Ask the user age
+# user_age = input("what is your age?")
+# #Print combine all the stuff
+# print("Hello " + user_name + ",you are" + user_age)
+#-------------------------------------------------------------
+
+#Functions
+
+# def say_hello():
+#     print("Hello")
+#     print("test")
+#     print("ketankulkarni")
+#
+#
+# say_hello()
+# say_hello()
+# say_hello()
+#-------------------------------------------------------------
+
+def say_hello(user_name,user_age):
+    print("Hello " + user_name + "you are " + str(user_age))
+
+def double_number(number):
+    result = number * 2
+    return result
+
+def print_double_number(number):
+    result = double_number(number)
+    print(result)
+
+print(double_number(4))
+print(double_number(3))
+print(result)
+
+a = 2
+
+# say_hello("Ketan", 34)
+# say_hello("Kunal",30)
+# say_hello("Jyoti",60)
+# say_hello("Deepak",65)
+
