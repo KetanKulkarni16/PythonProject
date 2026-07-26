374dd1fdb7d3761c236c0b2c046be2ab08071e1e
ketandeepakkulkarni <kulkarni.kd16@gmail.com>
2026-07-26 17:58:27 +0530

    Udemy 2026 New Short Course

---
+++ Python YouTube/Two.py
@@ -0,0 +1,45 @@
+# # # # #user_age = int(input("Enter age: "))
+# # # # month = input("Enter month: ")
+# # # #
+# # # # #if user_age >= 18:
+# # # #    #print("You are adult")
+# # # #
+# # # # #else:
+# # # #     #print("You are not an adult yet")
+# # # #
+# # # # if month in ['June','Jully','August']:
+# # # #     print("Summer Time")
+# # # #
+# # # # elif month in ['September','October','November']:
+# # # #     print("Atumn Time")
+# # # #
+# # # # elif month in ['December','January', 'February','March','April','May']:
+# # # #     print("Spring Time")
+# # # #
+# # # # print("End of program")
+# # # ###########################################################
+# # #
+# # # # for i in range(100):
+# # # #     print("Hello" + str(i))
+# # # #     print("test")
+# # #
+# # # i = 0
+# # # while i < 10:
+# # #     print("Hello" + str(i))
+# # #     i +=1
+# # #############################################################
+# #
+# # number_list =[3,4,5,12,-3,-7]
+# # for number in number_list:
+# #     print(number)
+# #     if numebr < 0:
+# #         print("number is negative")
+# ##############################################################
+#
+#
+# def max_value():
+#     values = [1,23,4,5,6]
+#     return max(values)
+#
+# print(max_value())
+
